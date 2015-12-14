26e3e9e57cedeb0468606d1484378f788293199b
Anton Eliasson <devel@antoneliasson.se>
2015-12-14 13:12:51 +0100

    Statement: parse done

--- parser/Statement.hs
+++ parser/Statement.hs
@@ -44,5 +44,5 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     else exec (elseStmts: stmts) dict input
 
 instance Parse Statement where
-  parse = error "Statement.parse not implemented"
+  parse = statement
   toString = error "Statement.toString not implemented"
